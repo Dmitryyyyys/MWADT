dafeaad4bb4c5de8154bc95d35c234d0efb816bd
Dmitryyyyys <118775938+Dmitryyyyys@users.noreply.github.com>
2024-12-26 08:30:38 +0300

    add second lab

---
+++ lab2a/lab2a/Controllers/ParmController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab2a.Controllers
+{
+    [Route("Parm")]
+    public class ParmController : Controller
+    {
+        [Route("{action}/{Id?}")]
+        public IActionResult Index(string Id)
+        {
+            ViewBag.Id = Id;  return View();
+        }
+        [Route("{action}/{Id?}")]
+        public IActionResult Uri01(int Id)
+        {
+            ViewBag.Id = Id;  return View();
+        }
+        [Route("{action}/{Id?}")]
+        public IActionResult Uri02(int? Id)
+        {
+            ViewBag.Id = Id;  return View();
+        }
+        [Route("{action}/{Id?}")]
+        public IActionResult Uri03(float Id)
+        {
+            ViewBag.Id = Id;  return View();
+        }
+        [Route("{action}/{Id?}")]
+        public IActionResult Uri04(DateTime? Id)
+        {
+            ViewBag.Id = Id;  return View();
+        }
+    }
+}
---
+++ lab2a/lab2a/Controllers/StartController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace lab2a.Controllers
+{
+    public class StartControlle:Controller
+    {
+    [Route("Start/Index")]
+        public IActionResult Index()
+        { 
+        return View("/Views/Start/Index.cshtml");
+        }
+        [Route("Start/One")]
+
+        public IActionResult One()
+        {
+            return Ok("Start/One");
+        }
+        [Route("Start/Two")]
+
+        public IActionResult Two()
+        {
+            return Ok("Start/Two");
+        }
+        [Route("Start/Three")]
+        public IActionResult Three()
+        {
+            return Ok("Start/Three");
+        }
+    }
+}
